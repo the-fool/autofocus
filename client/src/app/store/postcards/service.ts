893fa862db68787eb9f58de2ad8e462cc851a32e
Thomas Ruble <sketchbang@gmail.com>
2019-06-17 15:33:06 -0500

    add assets

--- client/src/app/store/postcards/service.ts
+++ client/src/app/store/postcards/service.ts
@@ -1,32 +1,7 @@
-import { of } from 'rxjs'
 import { Injectable } from '@angular/core'
 import { Postcard } from './models'
 import { HttpClient } from '@angular/common/http'
 import { environment } from 'src/environments/environment'
-
-const DUMMY: Postcard[] = [
-    {
-        title: 'Lake of Diana',
-        x: 0.5,
-        y: 0.6,
-        img: '/assets/diana.jpg',
-        comment: 'It lies in a small crater-like hollow on the mountainside'
-    },
-    {
-        title: 'Brahmapura',
-        x: 0.8,
-        y: 0.4,
-        img: '/assets/brahmapura.jpg',
-        comment: 'Brahmapura is a planet composed entirely of Brahman, considered superior to svarga and is full of eternity, knowledge and bliss, the planet of the Bhagavān.'
-    },
-    {
-        title: 'Starbucks',
-        x: 0.2,
-        y: 0.2,
-        img: '/assets/starbucks.jpg',
-        comment: 'Fast & Reliable'
-    },
-]
 
 @Injectable({
     providedIn: 'root'
@@ -34,6 +9,6 @@ const DUMMY: Postcard[] = [
 export class PostcardService {
     constructor(private http: HttpClient) {}
     getAll() {
-        return this.http.get(environment.postcardApiUrl)
+        return this.http.get<Postcard[]>(environment.postcardApiUrl + '/postcards')
     }
 }
--- deploy_server.sh
+++ deploy_server.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-gcloud builds submit server --tag gcr.io/autofocus/helloworld
-gcloud beta run deploy helloworld --image gcr.io/autofocus/helloworld
+gcloud builds submit server --tag gcr.io/autofocus/postcard-server
+gcloud beta run deploy postcard-server  --image gcr.io/autofocus/postcard-server --allow-unauthenticated
